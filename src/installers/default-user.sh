2a56d8c18ae5e786b57e5eda30f0701d15948240
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2023-08-28 17:21:15 +0200

    Fixes

--- src/installers/default-user.sh
+++ src/installers/default-user.sh
@@ -5,14 +5,22 @@
 #
 ###############################################################################
 
+set -e
+
 source lib/common.sh
 
 USERNAME=${1:-developer}
 PASSWORD=welcome
 
 if ! grep -q "1000:1000" /etc/passwd; then
-    echo "User with UID 1000 was not found, creating...:"
-    useradd -m -p "${PASSWORD}" -G sudo -s /bin/bash "${USERNAME}"
+    echo "User with UID 1000 was not found, creating..."
+    useradd -m -p "$(openssl passwd -1 "$PASSWORD")" -G sudo -s /bin/bash "${USERNAME}"
+
+    echo "Updating /etc/wsl.conf, setting default user."
+    cat >/etc/wsl.conf <<EOF
+[user]
+default = ${USERNAME}
+EOF
 else
     echo "Found user with default UID, doing nothing."
 fi
