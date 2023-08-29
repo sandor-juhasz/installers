38937c0f2b5e7902d46a647400ff91c7ea77d732
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2023-08-29 14:22:32 +0200

    Fixes

--- src/installers/default-user.sh
+++ src/installers/default-user.sh
@@ -15,7 +15,9 @@ PASSWORD=welcome
 if ! grep -q "1000:1000" /etc/passwd; then
     echo "User with UID 1000 was not found, creating..."
     useradd -m -p "$(openssl passwd -1 "$PASSWORD")" -G sudo -s /bin/bash "${USERNAME}"
-
+    
+    echo "Disabling Sudo password for $USERNAME..."
+    echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
     echo "Updating /etc/wsl.conf, setting default user."
     cat >/etc/wsl.conf <<EOF
 [user]
--- src/installers/utilities.sh
+++ src/installers/utilities.sh
@@ -7,5 +7,5 @@ set -e
 
 source lib/common.sh
 apt_install wget jq
-wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O ~/.local/bin/yq
-chmod +x ~/.local/bin/yq
+wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq
+chmod +x /usr/local/bin/yq
