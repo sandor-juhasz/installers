b6a5a867335b5134f2a9c1edb47bb793614890da
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2023-08-31 14:08:59 +0200

    Adding vscode plugin installer

--- src/features/bash-development-tools.sh
+++ src/features/bash-development-tools.sh
@@ -10,7 +10,10 @@
 #    USERNAME                The user name of the user for which the feature is installed.
 #
 
+source lib/common.sh
+
 export INSTALL_BASH_KERNEL=${INSTALL_BASH_KERNEL:-"true"}
+export INSTALL_VSCODE_EXTENSIONS=${INSTALL_VSCODE_EXTENSIONS:-"false"}
 
 function install() {
     # todo: install basic Python runtime if not installed.
@@ -25,6 +28,17 @@ function install() {
         fi
         installers/bash_kernel.sh "${USERNAME}"
     fi
+
+    # #
+    # # NOTE: On WSL, the platform VSCode cannot be executed if it is invoked through su / sudo.
+    # #       Updagint the PATH is not 
+    # #
+    # if [[ "${INSTALL_VSCODE_EXTENSIONS}" = "true" ]]; then
+    #     echo "Installing VSCode extensions..."
+    #     as_user "code --install-extension 'timonwong.shellcheck'"
+    #     as_user "code --install-extension 'rogalmic.bash-debug'"
+    #     as_user "code --install-extension 'ms-toolsai.jupyter'"
+    # fi
 }
 
 install
