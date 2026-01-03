3b7004ce65aea7959335bbdee69447b2887966ed
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2026-01-03 12:43:45 +0000

    Adding devcontainer-cli installer

---
+++ src/installers/devcontainer-cli.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+###############################################################################
+#
+# Installs Devcontainer CLI
+#
+###############################################################################
+
+set -e
+
+USERNAME=${1:$(id -un)}
+
+source lib/common.sh
+
+cat <<EOF
+Installing AWS CDK...
+=====================
+Parameters:
+   USERNAME: $USERNAME
+
+EOF
+
+su --login "$USERNAME" <<'EOF'
+    npm install -g @devcontainers/cli
+EOF
+
+echo "Done."
